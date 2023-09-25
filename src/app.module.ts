545dd8e504297a1060cd579c8f60a35982e4d2db
Anastasiia Zaostrovskaya <zasotrovskaya.asya@yandex.ru>
2023-09-25 11:47:35 +0800

    change db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { UsersModule } from './routes/users/users.module';
-
-import {
-  DB_TYPE,
-  PG_DB,
-  PG_HOST,
-  PG_PASSWORD,
-  PG_PORT,
-  PG_USERNAME,
-} from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: DB_TYPE as any,
-      host: PG_HOST,
-      port: PG_PORT,
-      username: PG_USERNAME,
-      password: PG_PASSWORD,
-      database: PG_DB,
-      entities: [__dirname + '/**/*.entity.{ts,js}'],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('PG_HOST'),
+        port: parseInt(configService.get<string>('PG_PORT')),
+        username: configService.get<string>('PG_USERNAME'),
+        password: configService.get<string>('PG_PASSWORD'),
+        database: configService.get<string>('PG_DB'),
+        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
