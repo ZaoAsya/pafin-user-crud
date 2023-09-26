c47854c3d5bfedcd5d2975600d765b6ea96ceed4
Anastasiia Zaostrovskaya <zasotrovskaya.asya@yandex.ru>
2023-09-26 21:48:39 +0800

    added auth.service test

--- src/routes/auth/tests/auth.service.spec.ts
+++ src/routes/auth/tests/auth.service.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from '../auth.service';
+import { JwtService } from '@nestjs/jwt';
+
+const jwtServiceMock = {
+  signAsync: jest.fn(),
+};
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: jwtServiceMock,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('getToken', () => {
+    it('should generate an access token', async () => {
+      const mockAccessToken = 'mock-access-token';
+      jwtServiceMock.signAsync.mockResolvedValue(mockAccessToken);
+
+      const username = 'testuser';
+      const result = await authService.getToken(username);
+
+      expect(result).toEqual({ access_token: mockAccessToken });
+      expect(jwtServiceMock.signAsync).toHaveBeenCalledWith({ username });
+    });
   });
 });
